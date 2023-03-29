760dbf4f3ce3c99140f03d13ec1906551f1456d8
Abir <abirhossain9@iut-dhaka.edu>
2023-03-29 16:39:48 +0600

    Completes the module

--- 1. Dependency Injection/src/main/java/com/example/dependencyInjection/MyApp.java
+++ 1. Dependency Injection/src/main/java/com/example/dependencyInjection/MyApp.java
@@ -1,14 +1,23 @@
 package com.example.dependencyInjection;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MyApp {
 
 	public static void main(String[] args) {
 
-		// create the object
-		Coach theCoach = new TrackCoach();
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+
+
 
 		// use the object
 		System.out.println(theCoach.getDailyWorkout());
+
+		System.out.println(theCoach.getDailyFortune());
 	}
 
 }
